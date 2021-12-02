8d4297404325360736fee93b5ff751af922ce9ef
moritzfelipe <stellmacher@gmail.com>
2021-12-02 17:46:07 +0100

    Update contract naming and amount
    Former-commit-id: d596da65f8e200d2e68dd7e97c7f0ca95615de9b

--- TokenCourse/erc20-token-creation/erc20TokenCreation.sol
+++ TokenCourse/erc20-token-creation/erc20TokenCreation.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract EduCoin is ERC20 {
-    constructor() ERC20("EduCoin", "EDC") {
-        _mint(msg.sender, 1000 * 10 ** decimals());
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
